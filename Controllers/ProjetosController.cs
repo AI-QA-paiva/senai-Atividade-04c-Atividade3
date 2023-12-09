e2777824ee7ef46bc132c1c20d673e0f78587863
AI-QA-paiva <pedropaiva.62124@gmail.com>
2023-12-09 17:47:08 -0300

    atualizando projeto, inserindo novos métodos/ações nas classes Controller e Repository

--- Controllers/ProjetosController.cs
+++ Controllers/ProjetosController.cs
@@ -23,5 +23,55 @@ namespace Exo.WebApi.Controllers
         {
             return Ok(_projetoRepository.Listar());
         }
+
+        //complementando a entrega incluindo itens restantes do crud
+        [HttpPost]
+        public IActionResult Cadastrar(Projeto projeto)
+        {
+            _projetoRepository.Cadastrar(projeto);
+            return StatusCode(201);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult BuscarPorId(int id)
+        {
+            Projeto registroPedido = _projetoRepository.BuscarporId(id); //cria uma variavel para receber o id requisitado no Json
+
+            if(registroPedido == null)//faz a logica para caso o id não exista;  se existir não executar o bloco de codigo dentro de if
+            {
+                return NotFound();//se não existir informa não encontrado
+            }
+            return Ok(registroPedido);//se existir retorna processo executado com sucesso
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Atualizar(int id, Projeto projeto)
+        {
+            _projetoRepository.Atualizar(id, projeto);
+            return StatusCode(204);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Apagar(int id)
+        {
+            try{
+                _projetoRepository.Deletar(id);
+                return StatusCode(204);
+            }
+            catch(Exception tratandoErro)
+            {
+                return BadRequest();
+            }
+        }
+
+
+
+
+
+
+
+
+
+
     }
 }
--- Repositories/ProjetoRepository.cs
+++ Repositories/ProjetoRepository.cs
@@ -20,5 +20,39 @@ namespace Exo.WebApi.Repositories
         {
             return _context.Projetos.ToList();
         }
+
+        //complementando a atividade, inserindo o CRUD
+        public void Cadastrar(Projeto projeto)
+        {
+            _context.Projetos.Add(projeto);
+            _context.SaveChanges();
+        }
+
+        public Projeto BuscarporId(int id)
+        {
+            return _context.Projetos.Find(id);
+        }
+
+        public void Atualizar(int id, Projeto projeto)
+        {
+            Projeto buscouIdnum = _context.Projetos.Find(id); //pra checar se o id existe, crio uma variavel para receber o valor id requisitado
+
+            if(buscouIdnum != null) //lógica para identificar se o id existe ou não
+            {
+                buscouIdnum.NomeDoProjeto = projeto.NomeDoProjeto; //se existir vai capturar os novos dados
+                buscouIdnum.Area = projeto.Area;
+                buscouIdnum.Status = projeto.Status;
+            }
+
+            _context.Projetos.Update(buscouIdnum); //irá atualizar os dados
+            _context.SaveChanges(); //ira salvar os dados
+        }
+
+        public void Deletar(int id)
+        {
+            Projeto qualId = _context.Projetos.Find(id);
+            _context.Projetos.Remove(qualId);
+            _context.SaveChanges();
+        }
     }
 }
